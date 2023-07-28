391ec875e221a78078d03d73add0743a05c0e7f1
Raghav9755 <raghav.agarwal975555@gmail.com>
2023-07-28 15:58:29 +0530

    Stack And Queue

---
+++ PracticeQuestionsJava/Stack/StackArraylist.java
@@ -0,0 +1,38 @@
+package PracticeQuestionsJava.Stack;
+import java.util.ArrayList;
+public class StackArraylist {
+    static ArrayList<Integer> list=new ArrayList<>();
+    public static boolean isEmpty(){
+        return list.size()==0;
+    }
+    public static void push(int data){
+        list.add(data);
+    }
+    public static int pop(){
+        if(isEmpty()){
+            return -1;
+        }
+        int top =list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+    }
+    public static int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return list.get(list.size()-1);
+    }
+
+public static void main(String args[]){
+    StackArraylist s=new StackArraylist();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    while(!isEmpty()){
+    System.out.println(s.peek());
+    s.pop();
+    }
+}
+}
